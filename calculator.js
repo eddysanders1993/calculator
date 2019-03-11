f3767c94a21339977bdae3eb2fd243227767924f
Edward Sanders <eddysanders1993@gmail.com>
2019-03-11 14:56:47 -0400

    performs and displays operations correctly some arithmetic incorrect, like 100-10+4/2*2 clear button doesnt clear all variables can continue operations on solution but only concats string

--- calculator.js
+++ calculator.js
@@ -11,9 +11,9 @@ let firstOperatorIndex = [];
 const container = document.querySelector('#screen');
 container.style.color = 'blue';
 const btnSeven = document.querySelector("#seven");
-btnSeven.onclick = () => display('7');
+btnSeven.onclick = () => display(7);
 const btnEight = document.querySelector("#eight");
-btnEight.onclick = () => display('8');
+btnEight.onclick = () => display(8);
 const btnNine = document.querySelector("#nine");
 btnNine.onclick = () => display(9);
 const btnFour = document.querySelector("#four");
@@ -29,7 +29,7 @@ btnTwo.onclick = () => display(2);
 const btnThree = document.querySelector("#three");
 btnThree.onclick = () => display(3);
 const btnZero = document.querySelector("#zero");
-btnZero.onclick = () => display('0');
+btnZero.onclick = () => display(0);
 const btnClear = document.querySelector("#clear");
 btnClear.onclick = () => clearDisplay();
 const btnPlus = document.querySelector("#add");
@@ -37,9 +37,9 @@ btnPlus.onclick = () => saveValues('+');
 const btnMinus = document.querySelector("#subtract");
 btnMinus.onclick = () => saveValues('-');
 const btnMultiply = document.querySelector("#multiply");
-btnMultiply.onclick = () => display('*');
+btnMultiply.onclick = () => saveValues('*');
 const btnDivide = document.querySelector("#divide");
-btnDivide.onclick = () => display('/');
+btnDivide.onclick = () => saveValues('/');
 const btnEqual = document.querySelector("#equals");
 btnEqual.onclick = () => saveAndOperate();
 
@@ -69,9 +69,9 @@ function clearDisplay() {
 	secondNumberArr = [];
 	firstOperator = '';
 	firstOperatorIndex = [];
-
 	container.textContent = '';
 }
+
 /*press operator
 all numbers before operator become firstNumber
 create firstNumberArray
@@ -79,13 +79,10 @@ join Array into firstNumberString when first operator pressed
 */
 
 function saveAndOperate() {
-
-
-finalNumberStr = numArr.join('');
-valueArr.push(finalNumberStr);
-operate(valueArr);
+	finalNumberStr = numArr.join('');
+	valueArr.push(finalNumberStr);
+	operate(valueArr);
 }
-
 
 function saveValues(x) {
 	displayValueArr.push(x);
@@ -97,42 +94,58 @@ function saveValues(x) {
 	valueArr.push(x);
 }
 
-
-
 function display(x) {
 	numArr.push(x);
 	displayValueArr.push(x);
 	displayValueStr = displayValueArr.join('');
 	container.textContent = displayValueStr;
-	}
-
+}
 
 function multiply (x,y) {
-return x * y;
+	return x * y;
 }
 
 function add (x,y) {
 	return x+y;
-
 }
 
 function subtract (x,y) {
-return x-y;
+	return x-y;
 }
 
 function divide (x,y) {
-return x / y;
+	return x / y;
 }
+
 
 function operate (x) {
-
-
-		while(valueArr.includes('+')) {
-		let plus = valueArr.indexOf('+')
-		return add(parseInt(valueArr[plus-1]),parseInt(valueArr[plus+1]));
+	while(valueArr.includes('*')) {
+		let multiplication = valueArr.indexOf('*')
+		valueArr.splice(multiplication-1,3,multiply(parseInt(valueArr[multiplication-1]),parseInt(valueArr[multiplication+1])));
 	}
-
+	while(valueArr.includes('/')) {
+		let division = valueArr.indexOf('/')
+		valueArr.splice(division-1,3,divide(parseInt(valueArr[division-1]),parseInt(valueArr[division+1])));
+	}
+	while(valueArr.includes('+')) {
+		let addition = valueArr.indexOf('+')
+		valueArr.splice(addition-1,3,add(parseInt(valueArr[addition-1]),parseInt(valueArr[addition+1])));
+	}
+	while(valueArr.includes('-')) {
+		let subtraction = valueArr.indexOf('-')
+		valueArr.splice(subtraction-1,3,subtract(parseInt(valueArr[subtraction-1]),parseInt(valueArr[subtraction+1])));
+	}
+	displayValueStr = valueArr.join('');
+	return container.textContent = displayValueStr;
 }
+
+
+
+
+
+
+
+
 //only performs operations on first two numbers
 // need to replace elements in array with solution
 
